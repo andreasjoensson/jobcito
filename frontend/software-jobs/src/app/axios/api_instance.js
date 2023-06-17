44d41795e9fd17f73422283736a0b53e747e949d
Andreas Moreno Jønsson <andreas.ecuador@live.dk>
2023-06-17 14:24:17 +0200

    It jobs side færdig

--- frontend/software-jobs/src/app/axios/api_instance.js
+++ frontend/software-jobs/src/app/axios/api_instance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
 
 const instance = axios.create({
-  baseURL: "http://localhost:3003",
+  baseURL: apiEndpoint,
 });
 
 export default instance;
--- frontend/software-jobs/src/app/home/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import instance from "../axios/api_instance";
-
-export default function home() {
-  return <div>hey</div>;
-}
